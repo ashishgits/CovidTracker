adbfe7cbb4559e56e50fac68384daa65b30189ad
Ashish Bhandari <ashishb0410@gmail.com>
2022-06-28 14:26:04 +0530

    Improved design and API

--- src/components/Covid.js
+++ src/components/Covid.js
@@ -3,13 +3,15 @@ import "./covid.css";
 
 const Covid = () => {
   const [data, setData] = useState([]);
+  const [data2, setData2] = useState([]);
 
   const getCovidData = async () => {
     try {
       const res = await fetch("https://covid-api.mmediagroup.fr/v1/cases");
       const actualData = await res.json();
       console.log(actualData.India);
-      setData(actualData.India);
+      setData(actualData.India.All);
+      setData2(actualData.India.Delhi);
     } catch (err) {
       console.log(err);
     }
@@ -31,18 +33,6 @@ const Covid = () => {
                   <span className="blue"> OUR </span>COUNTRY
                 </p>
                 <p className="card__total card__small blue">INDIA</p>
-              </div>
-            </div>
-          </li>
-          <li className="card green">
-            <div className="card__main">
-              <div className="card__inner green">
-                <p className="card__name green">
-                  <span className="green"> TOTAL </span>RECOVERED
-                </p>
-                <p className="card__total card__small green">
-                  {data.recovered}
-                </p>
               </div>
             </div>
           </li>
@@ -68,13 +58,15 @@ const Covid = () => {
               </div>
             </div>
           </li>
-          <li className="card blue">
+          <li className="card green">
             <div className="card__main">
-              <div className="card__inner blue">
-                <p className="card__name blue">
-                  <span className="blue"> TOTAL </span>ACTIVE
+              <div className="card__inner green">
+                <p className="card__name green">
+                  <span className="green"> LIFE </span>EXPECTANCY
                 </p>
-                <p className="card__total card__small blue">{data.active}</p>
+                <p className="card__total card__small green">
+                  {data.life_expectancy}
+                </p>
               </div>
             </div>
           </li>
@@ -84,9 +76,7 @@ const Covid = () => {
                 <p className="card__name brown">
                   <span className="brown"> LAST </span>UPDATED
                 </p>
-                <p className="card__total card__small brown">
-                  {/* {data.India.Delhi.updated} */}
-                </p>
+                <p className="card__total card__small brown">{data2.updated}</p>
               </div>
             </div>
           </li>
